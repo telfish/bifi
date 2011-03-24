84cb8ec24833902d6dcf5f6e862547e025fc5440
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-03-24 17:40:37 +0100

    build: test-prepare

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,4 +2,6 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.7" % "test" withSources()
+
+  lazy val testPrepare = task { None } dependsOn(copyTestResources, testCompile)
 }
