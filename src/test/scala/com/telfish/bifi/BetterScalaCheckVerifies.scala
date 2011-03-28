ce7f1e00c6270e58ae03761cea346478d8eee1f7
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-03-28 12:19:12 +0200

    properly use Prop.forAll

--- src/test/scala/com/telfish/bifi/BetterScalaCheckVerifies.scala
+++ src/test/scala/com/telfish/bifi/BetterScalaCheckVerifies.scala
@@ -15,7 +15,7 @@ trait BetterScalaCheckVerifies {
   case class VerifiableExpectation2(e: String) {
     def verifies[A1: Arbitrary : Shrink, A2: Arbitrary : Shrink, P <% Prop](p: (A1, A2) => P) =
       forExample(e) in {
-        p.must(pass(Prop.forAll(_: (A1, A2) => P) ) )
+        Prop.forAll(p) must pass
       }
   }
 }
