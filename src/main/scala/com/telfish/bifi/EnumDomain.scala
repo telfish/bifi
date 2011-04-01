edba8dbb8c79153b7ac75bd1934d24f7ba8e23e1
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-04-01 09:14:29 +0200

    add a `~`-operator to chain SetExpr

--- src/main/scala/com/telfish/bifi/EnumDomain.scala
+++ src/main/scala/com/telfish/bifi/EnumDomain.scala
@@ -8,6 +8,14 @@ object SetExpr {
   case class Single[T](t: T) extends SetExpr[T]
   case class Several[T](t: List[T]) extends SetExpr[T]
   case object All extends SetExpr[Nothing]
+
+  trait HasTuplizer[X] {
+    def ~[U](next: U): (X, U)
+  }
+
+  implicit def tuplize[T](x: T): HasTuplizer[T] = new HasTuplizer[T] {
+    def ~[U](next: U): (T, U) = (x, next)
+  }
 }
 
 class EnumDomain[T](values: IndexedSeq[T]) extends RangeDomain[T, SetExpr[T]] {
