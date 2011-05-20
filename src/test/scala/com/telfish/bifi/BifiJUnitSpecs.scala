dad5d41aff9332f56d14d18c7fe82528ecbb1758
Johannes Rudolph <johannes.rudolph@googlemail.com>
2011-05-20 12:08:02 +0200

    bifi: rename JUnitSpecs

--- src/test/scala/com/telfish/bifi/BifiJUnitSpecs.scala
+++ src/test/scala/com/telfish/bifi/BifiJUnitSpecs.scala
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith
 import org.specs.runner.{JUnit4, JUnitSuiteRunner}
 
 @RunWith(classOf[JUnitSuiteRunner])
-class JUnitSpecs extends JUnit4(
+class BifiJUnitSpecs extends JUnit4(
   LongRangeMapSpecs,
   domain.DomainSpecs, domain.ConcreteDomainSpecs, domain.DomainMapSpecs
 )
