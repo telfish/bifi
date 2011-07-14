c0fba0fca02c234f1354b182b467c4d4604910ed
Johannes Rudolph <johannes.rudolph@googlemail.com>
2011-07-14 15:36:48 +0200

    update to Scala 2.9.0-1 + dependencies

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,10 +1,8 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val snapshots = ScalaToolsSnapshots
-
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.8-SNAPSHOT" % "test" withSources()
-  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.8" % "test" withSources()
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test" withSources()
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test" withSources()
   lazy val junit = "junit" % "junit" % "4.4" % "test" withSources() // for running specs with IDEA
 
   override def compileOptions = CompileOption("-unchecked") :: super.compileOptions.toList
