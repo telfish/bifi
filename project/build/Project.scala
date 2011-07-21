bd445b801ed35877aea1f3cddbc116f342c8ac02
Johannes Rudolph <johannes_rudolph@gmx.de>
2011-07-21 10:23:35 +0200

    build: use telfish sbt plugin for project definition

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,19 +1,5 @@
 import sbt._
 
-class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test" withSources()
-  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test" withSources()
-  lazy val junit = "junit" % "junit" % "4.4" % "test" withSources() // for running specs with IDEA
-
-  override def compileOptions = CompileOption("-unchecked") :: super.compileOptions.toList
+class Project(info: ProjectInfo) extends DefaultProject(info) with TelfishProject {
   override def testCompileOptions = CompileOption("-Ydependent-method-types") :: super.testCompileOptions.toList
-
-  lazy val testPrepare = task { None } dependsOn(copyTestResources, testCompile)
-
-  // Additional JUnit XML report generation support
-  import eu.henkelmann.sbt.JUnitXmlTestsListener
-  //create a listener that writes to the normal output directory
-  def junitXmlListener: TestReportListener = new JUnitXmlTestsListener(outputPath.toString)
-  //add the new listener to the already configured ones
-  override def testListeners: Seq[TestReportListener] = super.testListeners ++ Seq(junitXmlListener)
 }
--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,12 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  object Repositories {
-    val JUnitXMLReportRepo= MavenRepository("Christoph's Maven Repo", "http://maven.henkelmann.eu/")
-  }
-
-  import Repositories._
-  lazy val junitXMLReportModuleConfig = ModuleConfiguration("eu.henkelmann", JUnitXMLReportRepo)
-
-  lazy val jUnitXMLReport = "eu.henkelmann" % "junit_xml_listener" % "0.2"
+  val TelfishNexus = "Telfish Nexus" at "http://silo/content/repositories/releases/"
+  val telfish = "com.telfish" % "sbt-plugin" % "1.2.1"
 }
